59b31335a531c2c5ecc8125d0a4a31aa3735eacf
gibsonbailey <gibson.bailey@live.com>
2023-01-07 21:47:39 -0800

    Create basic request to get contribution data.

---
+++ Git Streak Tracker Widget/Contributions.swift
@@ -0,0 +1,108 @@
+//
+//  Contributions.swift
+//  Git Streak Tracker
+//
+//  Created by Bailey Lind-Trefts on 1/7/23.
+//
+
+import Foundation
+
+
+struct ResponseData : Decodable {
+    let data: BaseData
+}
+
+struct BaseData : Decodable {
+    let user: UserData
+}
+
+struct UserData : Decodable {
+    let contributionsCollection: ContributionsCollection
+}
+
+struct ContributionsCollection : Decodable {
+    let contributionCalendar: ContributionCalendar
+}
+
+struct ContributionCalendar : Decodable {
+    let weeks: [Week]
+}
+
+struct Week : Decodable {
+    let contributionDays: [ContributionDay]
+}
+
+struct ContributionDay : Decodable {
+    let contributionCount: Int
+    let weekday: Int
+    let date: String
+}
+
+func fetchContributions() -> ResponseData? {
+    let url = URL(string: "http://localhost:5000/")!
+    var data: Data?
+    let semaphore = DispatchSemaphore(value: 0)
+    
+    let task = URLSession.shared.dataTask(with: url) { (d, _, _) in
+        data = d
+        semaphore.signal()
+    }
+    task.resume()
+    
+    semaphore.wait()
+    
+    guard let successData = data else { return nil }
+    
+    print(successData)
+    
+    do {
+        let todo = try JSONDecoder().decode(ResponseData.self, from: successData)
+        print("x")
+        return todo
+    } catch {
+        return nil
+    }
+}
+
+struct ContributionData {
+    let streakLength: Int
+    let todayComplete: Bool
+}
+
+func getContributions() -> Void {
+    print("Getting Contributions")
+    guard let contributionData = fetchContributions() else { return }
+//    print(contributionData)
+    var currentDateTime = Date()
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    let todaysDateString = dateFormatter.string(from: currentDateTime)
+    currentDateTime = dateFormatter.date(from: todaysDateString)!
+    print(todaysDateString)
+    var streakLength = 0
+    var streakBroken = false
+    var todayComplete = false
+    contributionData.data.user.contributionsCollection.contributionCalendar.weeks.reversed().forEach { week in
+        week.contributionDays.reversed().forEach { day in
+            let date = dateFormatter.date(from: day.date)!
+            if date == currentDateTime {
+                if day.contributionCount > 0 {
+                    todayComplete = true
+                    streakLength += 1
+                }
+            } else if date < currentDateTime {
+                if day.contributionCount > 0 {
+                    if !streakBroken {
+                        streakLength += 1
+                    }
+                } else {
+                    streakBroken = true
+                }
+            }
+        }
+    }
+    print(todayComplete)
+    print(streakLength)
+    return
+}
+
