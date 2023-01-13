0375ef69171a3c29ce66a7b4c60b3dbda1184b5f
Samuel Wood <samote.wood@gmail.com>
2023-01-12 20:42:54 -0800

    xcodproj file added

---
+++ Git Streak Tracker Widget/ThemeConfig.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeConfig.swift
+//  Git Streak Tracker
+//
+//  Created by Samuel Wood on 1/12/23.
+//
+
+import Foundation
--- ThemeConfig.swift
+++ ThemeConfig.swift
@@ -1,3 +1,10 @@
+//
+//  ThemeConfig.swift
+//  Git Streak Tracker
+//
+//  Created by Samuel Wood on 1/12/23.
+//
+
 import Foundation
 import SwiftUI
 
