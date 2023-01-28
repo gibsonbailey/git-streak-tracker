3834121173cf342952507e4f4e344f3bdb3f240f
gibsonbailey <baileytrefts@gmail.com>
2023-01-28 14:01:33 -0800

    Simplify api views by using common validator.

--- web/pages/api/contributions/[githubUsername].ts
+++ web/pages/api/contributions/[githubUsername].ts
@@ -1,20 +1,15 @@
 import { getGithubContributions } from "../../../utils/githubContributions"
+import { validateGitHubUsername } from "../../../utils/validation"
 
 
 export default async function handler(req, res) {
     const { githubUsername } = req.query
 
-    if (!githubUsername) {
-        // TODO: Not sure how this would ever happen. Empty url parameter?
-        throw Error('githubUsername query parameter required.')
-    } else {
-        // Reject if username contains anything but alphanumeric and dash characters
-        if (!/^[a-zA-Z0-9-]*$/.test(githubUsername)) {
-            // TODO: This serves 400 page. Probably not what we want. We want to serve the simple error message below.
-            res.status(400)
-            res.end('githubUsername must be a valid GitHub username.',)
-        }
+    if (!validateGitHubUsername(githubUsername)) {
+        res.status(400)
+        res.end('url parameter githubUsername must be a valid GitHub username.',)
     }
+
     const contributionData = await getGithubContributions(githubUsername)
     res.end(JSON.stringify(contributionData))
 }
