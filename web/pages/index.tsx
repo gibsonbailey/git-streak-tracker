398c4ac50dddba3f546a746ea0accc77c68d58ab
gibsonbailey <baileytrefts@gmail.com>
2023-01-30 16:22:57 -0800

    Utilize non-deprecated properties on next Image component.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -13,8 +13,7 @@ export const HomePage = () => (
     <Image
       alt='fire'
       src='/copper.svg'
-      layout='fill'
-      objectFit='contain'
+      fill
       className={styles.backgroundImage}
     />
   </>
