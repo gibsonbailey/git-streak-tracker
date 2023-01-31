4bee7be4c95092e1d59102f2ab1499cb8021089b
Samuel Wood <samote.wood@gmail.com>
2023-01-31 13:41:03 -0800

    homepage updates

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,9 +6,39 @@ export const HomePage = () => (
     <div
       className={styles.container}
     >
-      <h1
-        className={styles.title}
-      >Git Streak Tracker</h1>
+      <div className="mr-6">
+        <h1
+          className="text-3xl font-bold text-white mb-8"
+        >
+          Git Streak Tracker
+        </h1>
+        <a>
+          <button className="border-white border-2 rounded-xl cursor-pointer">
+            <Image
+              alt='Iphone outline'
+              src='/App-Store-Button.png'
+              width="200"
+              height="50"
+            />
+          </button>
+        </a>
+      </div>
+      <div className={styles.iphoneContainer}>
+        <Image
+          alt='Iphone outline'
+          src='/iphone-14-outline.svg'
+          fill
+        />
+        <Image
+          alt='Iphone outline'
+          src='/profile-screen.png'
+          width={100}
+          height={100}
+          priority
+          unoptimized={true}
+          className={styles.profileScreen}
+        />
+      </div>
     </div>
     <Image
       alt='fire'
