a66f9da51591a16410b7763870927ca67e9dca6b
gibsonbailey <baileytrefts@gmail.com>
2023-01-31 20:08:21 -0800

    Add Cache-Control header to markdown image.

--- web/pages/api/markdown_image/[githubUsername].ts
+++ web/pages/api/markdown_image/[githubUsername].ts
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
     })
 
     res.setHeader('Content-Type', 'image/svg+xml')
+    res.setHeader('Cache-Control', 'no-cache')
     res.end(svgData)
 }
 
