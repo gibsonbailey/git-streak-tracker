0f7c0e98cc8894bb84918039f96eb317cf39d8a5
gibsonbailey <baileytrefts@gmail.com>
2023-01-31 23:17:38 -0800

    Use lower-cased cache-control header.

--- web/pages/api/markdown_image/[githubUsername].ts
+++ web/pages/api/markdown_image/[githubUsername].ts
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
     })
 
     res.setHeader('Content-Type', 'image/svg+xml')
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
+    res.setHeader('cache-control', 'no-cache, no-store, must-revalidate')
     res.end(svgData)
 }
 
