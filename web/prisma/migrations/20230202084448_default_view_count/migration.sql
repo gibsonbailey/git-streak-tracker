92af61e53810d0ee593f631b82ef47275012f0a2
gibsonbailey <baileytrefts@gmail.com>
2023-02-02 01:32:56 -0800

    Add prisma schema and migrations.

---
+++ web/prisma/migrations/20230202084448_default_view_count/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "view_count" SET DEFAULT 0;
---
+++ web/prisma/migrations/20230202090653_lowercase_table_name/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "view_count" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
