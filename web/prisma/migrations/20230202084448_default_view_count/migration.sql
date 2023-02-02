c9dbf6126767e21981dc7d76452b172e9436b39d
gibsonbailey <baileytrefts@gmail.com>
2023-02-02 14:51:32 -0800

    Consolidate migrations.

--- web/prisma/migrations/20230202084448_default_view_count/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "view_count" SET DEFAULT 0;
--- web/prisma/migrations/20230202224350_init/migration.sql
+++ web/prisma/migrations/20230202224350_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
