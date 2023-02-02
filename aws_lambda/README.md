427e1c05361407a0a1467960799bff5c5bbf6fc8
gibsonbailey <baileytrefts@gmail.com>
2023-02-02 11:56:02 -0800

    Create lambda function with documentation.

---
+++ aws_lambda/README.md
@@ -0,0 +1,15 @@
+### AWS Lambda
+We use AWS Lambda to count views of our cloudfront distribution.
+
+
+Cloudfront is configured to periodically gzip a log file that contains many requests and push it to a dedicated S3 bucket. This function will be triggered whenever an object is created in our logging bucket. The function will then aggregate the data and make one query to our postgres database to UPSERT the new data.
+
+The web server is configured to read view counts (per GitHub username) from the same database.
+
+### Building
+When we update the lambda function code on AWS, we need to do it by sending a zip file. This is because we have python dependencies, e.g. psycopg.
+
+In order to build, follow this process:
+1. Make sure you've created a python virtual environment in the `aws_lambda/venv` directory.
+2. Activate the environment (`source venv/bin/activate`) and install the dependencies `pip install -r requirements.txt`.
+3. In the `aws_lambda` directory, run the `build.sh` script.
---
+++ aws_lambda/lambda_function.py
@@ -0,0 +1,57 @@
+import boto3
+import tempfile
+import gzip
+import shutil
+import csv
+
+
+HEADER_PREFIX = '#Fields: '
+
+
+def lambda_handler(event, context):
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    object_key = event['Records'][0]['s3']['object']['key']
+
+    print(bucket)
+    print(object_key)
+
+    try:
+        temp_dir = tempfile.TemporaryDirectory()
+        filename = f'{temp_dir.name}/logs.txt'
+        filename_gz = f'{filename}.gz'
+
+        s3 = boto3.client('s3')
+        s3.download_file(bucket, object_key, filename_gz)
+
+        with gzip.open(filename_gz, 'rb') as f_in:
+            with open(filename, 'wb+') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+
+        with open(filename, 'r') as f:
+            data = f.readlines()
+            data = data[1:]  # Remove first line. Does not contain data.
+            # Remove prefix to the list of fields on the header line.
+            data[0] = data[0].replace(HEADER_PREFIX, '').replace(' ', '\t')
+
+        with open(filename, 'w+') as f:
+            f.writelines(data)
+            f.seek(0)
+            reader = csv.DictReader(f, delimiter='\t')
+            for row in reader:
+                print(row['date'], row['cs-uri-stem'])
+
+        # Bulk upsert example that works:
+        #
+        # INSERT INTO users (username, view_count) VALUES
+        #     ('gibsonbailey', 1),
+        #     ('sw00d', 3)
+        # ON CONFLICT (username) DO UPDATE SET view_count = users.view_count + EXCLUDED.view_count;
+
+    except Exception as e:
+        print('Bad news! Failure!')
+        print(e)
+
+    return {
+        'statusCode': 200,
+        'body': "Hello!"
+    }
