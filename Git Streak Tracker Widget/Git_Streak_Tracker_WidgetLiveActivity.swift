b767fe0850a5d10e3e6b5b1ba829d8b962e39475
gibsonbailey <baileytrefts@gmail.com>
2023-02-10 22:02:03 -0800

    Support iOS 16.0+, rather than 16.2 only.

--- Git Streak Tracker Widget/Git_Streak_Tracker_WidgetLiveActivity.swift
+++ Git Streak Tracker Widget/Git_Streak_Tracker_WidgetLiveActivity.swift
@@ -19,59 +19,59 @@ struct Git_Streak_Tracker_WidgetAttributes: ActivityAttributes {
     var name: String
 }
 
-struct Git_Streak_Tracker_WidgetLiveActivity: Widget {
-    var body: some WidgetConfiguration {
-        ActivityConfiguration(for: Git_Streak_Tracker_WidgetAttributes.self) { context in
-            // Lock screen/banner UI goes here
-            VStack {
-                Text("Hello")
-            }
-            .activityBackgroundTint(Color.cyan)
-            .activitySystemActionForegroundColor(Color.black)
-
-        } dynamicIsland: { context in
-            DynamicIsland {
-                // Expanded UI goes here.  Compose the expanded UI through
-                // various regions, like leading/trailing/center/bottom
-                DynamicIslandExpandedRegion(.leading) {
-                    Text("Leading")
-                }
-                DynamicIslandExpandedRegion(.trailing) {
-                    Text("Trailing")
-                }
-                DynamicIslandExpandedRegion(.bottom) {
-                    Text("Bottom")
-                    // more content
-                }
-            } compactLeading: {
-                Text("L")
-            } compactTrailing: {
-                Text("T")
-            } minimal: {
-                Text("Min")
-            }
-            .widgetURL(URL(string: "http://www.apple.com"))
-            .keylineTint(Color.red)
-        }
-    }
-}
-
-struct Git_Streak_Tracker_WidgetLiveActivity_Previews: PreviewProvider {
-    static let attributes = Git_Streak_Tracker_WidgetAttributes(name: "Me")
-    static let contentState = Git_Streak_Tracker_WidgetAttributes.ContentState(value: 3)
-
-    static var previews: some View {
-        attributes
-            .previewContext(contentState, viewKind: .dynamicIsland(.compact))
-            .previewDisplayName("Island Compact")
-        attributes
-            .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
-            .previewDisplayName("Island Expanded")
-        attributes
-            .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
-            .previewDisplayName("Minimal")
-        attributes
-            .previewContext(contentState, viewKind: .content)
-            .previewDisplayName("Notification")
-    }
-}
+//struct Git_Streak_Tracker_WidgetLiveActivity: Widget {
+//    var body: some WidgetConfiguration {
+//        ActivityConfiguration(for: Git_Streak_Tracker_WidgetAttributes.self) { context in
+//            // Lock screen/banner UI goes here
+//            VStack {
+//                Text("Hello")
+//            }
+//            .activityBackgroundTint(Color.cyan)
+//            .activitySystemActionForegroundColor(Color.black)
+//
+//        } dynamicIsland: { context in
+//            DynamicIsland {
+//                // Expanded UI goes here.  Compose the expanded UI through
+//                // various regions, like leading/trailing/center/bottom
+//                DynamicIslandExpandedRegion(.leading) {
+//                    Text("Leading")
+//                }
+//                DynamicIslandExpandedRegion(.trailing) {
+//                    Text("Trailing")
+//                }
+//                DynamicIslandExpandedRegion(.bottom) {
+//                    Text("Bottom")
+//                    // more content
+//                }
+//            } compactLeading: {
+//                Text("L")
+//            } compactTrailing: {
+//                Text("T")
+//            } minimal: {
+//                Text("Min")
+//            }
+//            .widgetURL(URL(string: "http://www.apple.com"))
+//            .keylineTint(Color.red)
+//        }
+//    }
+//}
+//
+//struct Git_Streak_Tracker_WidgetLiveActivity_Previews: PreviewProvider {
+//    static let attributes = Git_Streak_Tracker_WidgetAttributes(name: "Me")
+//    static let contentState = Git_Streak_Tracker_WidgetAttributes.ContentState(value: 3)
+//
+//    static var previews: some View {
+//        attributes
+//            .previewContext(contentState, viewKind: .dynamicIsland(.compact))
+//            .previewDisplayName("Island Compact")
+//        attributes
+//            .previewContext(contentState, viewKind: .dynamicIsland(.expanded))
+//            .previewDisplayName("Island Expanded")
+//        attributes
+//            .previewContext(contentState, viewKind: .dynamicIsland(.minimal))
+//            .previewDisplayName("Minimal")
+//        attributes
+//            .previewContext(contentState, viewKind: .content)
+//            .previewDisplayName("Notification")
+//    }
+//}
