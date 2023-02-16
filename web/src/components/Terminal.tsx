d5d79ffccb986c57bbaeb9152566d6dd6cb10cae
gibsonbailey <baileytrefts@gmail.com>
2023-02-15 20:32:45 -0800

    Add basic vim view.

--- web/src/components/Terminal.tsx
+++ web/src/components/Terminal.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 
 export default () => {
     return (
-        <div className='w-full flex flex-col bg-gray-900 rounded-2xl'>
+        <div className='w-full flex flex-col bg-gray-900 rounded-2xl overflow-hidden'>
             <Header />
             <Content />
         </div>
@@ -38,7 +38,19 @@ drwxr-xr-x   25 git-man  staff    800 Feb 15 18:41 ..
 drwxr-xr-x   16 git-man  staff    512 Feb 14 14:06 .git
 -rw-r--r--    1 git-man  staff   1408 Feb  6 21:44 .gitignore
 -rw-r--r--    1 git-man  staff    154 Nov 13 12:03 code.ts
-    `
+`
+
+const vimText = `export default () => {
+    const [ mode, setMode ] = useState<'shell' | 'vim'>('shell')
+
+    return mode === 'vim' ? (
+        <div className='text-red-500'>VIM MODE</div>
+    ) : (
+        <div className='text-blue-500'>SHELL MODE</div>
+    )
+}
+`
+const vimStatusBar = `--NORMAL--`
 
 const Content = () => {
     const [ mode, setMode ] = useState<'shell' | 'vim'>('shell')
@@ -46,8 +58,18 @@ const Content = () => {
     const [ currentCommand, setCurrentCommand ] = useState('ls -la')
     const [ commandOutputs, setCommandOutputs ] = useState<string[]>([])
     const [ currentCommandCursorIndex, setCurrentCommandIndex ] = useState(0)
+    const [ pauseAnimation, setPauseAnimation ] = useState(true)
 
     useEffect(() => {
+        setTimeout(() => {
+            setPauseAnimation(false)
+        }, 1500)
+    }, [])
+
+    useEffect(() => {
+        if (pauseAnimation) {
+            return
+        }
         if (currentCommandCursorIndex < currentCommand.length) {
             setTimeout(() => {
                 setCurrentCommandIndex(currentCommandCursorIndex + 1)
@@ -65,11 +87,14 @@ const Content = () => {
                 setMode('vim')
             }
         }
-    }, [ currentCommandCursorIndex ])
+    }, [ currentCommandCursorIndex, pauseAnimation ])
 
     return (
         mode === 'vim' ? (
-            <div></div>
+            <div className='flex flex-col h-72 justify-between w-full'>
+                <pre className='p-2'>{vimText}</pre>
+                <div className='bg-cyan-800 text-slate-50 w-full p-1'>{vimStatusBar}</div>
+            </div>
         ) : (
             <div className='flex flex-col justify-end h-72 w-full'>
                 <div className='p-2'>
