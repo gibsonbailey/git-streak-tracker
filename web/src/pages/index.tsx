6ea0fa0f4ab49d4263ea6cc306c0fda47922902e
gibsonbailey <baileytrefts@gmail.com>
2023-02-19 14:53:21 -0800

    Add laser beam to home page.

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,37 +1,32 @@
 import Image from 'next/image'
 import Terminal from '../components/terminal'
 import styles from './index.module.css'
-import PhoneAnimation from "../components/PhoneAnimation/"
+import PhoneAnimation from '../components/PhoneAnimation/'
+import Laser from '../components/Laser'
 
 export const HomePage = () => (
   <>
-    <div className='flex h-full w-full absolute justify-center items-center z-10'>
-      <div className='mr-6 w-1/2 flex flex-col items-end'>
-        <h1 className="text-3xl font-bold mb-8">
-          Git Streak Tracker
-        </h1>
+    <div className="flex h-full w-full absolute justify-center items-center z-10">
+      <div className="w-1/2 flex flex-col items-end">
+        <h1 className="text-3xl font-bold mb-8">Git Streak Tracker</h1>
         <Terminal />
         <a>
           <button className="mt-8 border-white border-2 rounded-xl cursor-pointer">
             <Image
-              alt='Iphone outline'
-              src='/App-Store-Button.png'
+              alt="Iphone outline"
+              src="/App-Store-Button.png"
               width="200"
               height="50"
             />
           </button>
         </a>
       </div>
+      <Laser />
       <div className={styles.iphoneContainer}>
         <PhoneAnimation />
       </div>
     </div>
-    <Image
-      alt='fire'
-      src='/copper.svg'
-      fill
-      className='opacity-10 absolute'
-    />
+    <Image alt="fire" src="/copper.svg" fill className="opacity-10 absolute" />
   </>
 )
 
