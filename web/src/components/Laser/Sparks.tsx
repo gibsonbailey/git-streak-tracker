b7668050d8a907f2ef78cb31eafbc72e92bbd687
gibsonbailey <baileytrefts@gmail.com>
2023-02-23 22:38:26 -0800

    Dynamically calculate the origin position of the sparks based on the iphone frame's position.

--- web/src/components/Laser/Sparks.tsx
+++ web/src/components/Laser/Sparks.tsx
@@ -1,7 +1,20 @@
 import * as THREE from 'three'
-import { useRef, forwardRef, PropsWithChildren } from 'react'
-import { Canvas, useFrame, ThreeElements, extend } from '@react-three/fiber'
+import {
+  useRef,
+  forwardRef,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react'
+import {
+  Canvas,
+  useFrame,
+  ThreeElements,
+  extend,
+  useThree,
+} from '@react-three/fiber'
 import { EffectComposer, Bloom, GodRays } from '@react-three/postprocessing'
+import { createSemanticDiagnosticsBuilderProgram } from 'typescript'
 
 extend({ EffectComposer, Bloom, GodRays })
 
@@ -26,11 +39,7 @@ const Circle = forwardRef<THREE.Mesh>(
   ) => (
     <mesh ref={ref} {...props}>
       <circleGeometry args={[radius, segments]} />
-      <meshBasicMaterial
-        transparent={true}
-        opacity={opacity}
-        color={color}
-      />
+      <meshBasicMaterial transparent={true} opacity={opacity} color={color} />
       {children}
     </mesh>
   ),
@@ -58,10 +67,13 @@ const generateInitialVelocity = () => {
   ]
 }
 
-const initialXPosition = 3.64
-const initialYPosition = 0
+// const initialXPosition = 3.64
+// const initialYPosition = 0
 
-const generateParticle = () => {
+const generateParticle = (
+  initialXPosition: number,
+  initialYPosition: number,
+) => {
   // HSL bounds for green particles
   // h: 92, 129
   // s: 0%, 100%
@@ -83,110 +95,174 @@ const generateParticle = () => {
   }
 }
 
-const Particles = forwardRef(({}, controlRef) => {
-  const particleQuantity = 2000
-  const gravity = -0.002
-  const xDampening = 1 - 0.01
-  const floorHeight = -3.5
+const Particles = forwardRef(
+  (
+    {
+      xOriginPosition,
+      iPhoneFrameRef,
+    }: {
+      xOriginPosition: number
+      iPhoneFrameRef: React.RefObject<HTMLDivElement>
+    },
+    controlRef,
+  ) => {
+    const particleQuantity = 2000
+    const gravity = -0.002
+    const xDampening = 1 - 0.01
+    const floorHeight = -3.5
 
-  const groupRef = useRef<THREE.Group>()
-  const particleShown = useRef<number>(0)
-  const particles = useRef(
-    Array.from({ length: particleQuantity }, generateParticle),
-  )
+    const { camera } = useThree()
 
-  useFrame(() => {
-    if (!controlRef.current) {
-      return
-    }
-    if (groupRef.current) {
-      if (particleShown.current < particleQuantity) {
-        particleShown.current += 10
+    const [initialXPosition, setInitialXPosition] = useState(0)
+    const initialYPosition = 0
+
+    useEffect(() => {
+      var vFOV = THREE.MathUtils.degToRad(camera.fov)
+      var height = 2 * Math.tan(vFOV / 2) * camera.position.z // visible height
+      var worldWidth = height * camera.aspect
+      let x = worldWidth * (xOriginPosition - 0.5)
+      setInitialXPosition(x)
+      particles.current = Array.from({ length: particleQuantity }, () =>
+        generateParticle(initialXPosition, initialYPosition),
+      )
+    }, [xOriginPosition])
+
+    const groupRef = useRef<THREE.Group>()
+    const particleShown = useRef<number>(0)
+    const particles = useRef(
+      Array.from({ length: particleQuantity }, () =>
+        generateParticle(initialXPosition, initialYPosition),
+      ),
+    )
+
+    useFrame(() => {
+      if (!controlRef.current) {
+        return
       }
-      groupRef.current.children.forEach((particle, index) => {
-        // Gradually introduce particles
-        if (index > particleShown.current) {
-          return
+      if (groupRef.current) {
+        if (particleShown.current < particleQuantity) {
+          particleShown.current += 10
         }
+        groupRef.current.children.forEach((particle, index) => {
+          // Gradually introduce particles
+          if (index > particleShown.current) {
+            return
+          }
 
-        // Reset particle to origin
-        if (particle.material.opacity < 0.1) {
-          particles.current[index].velocity = generateInitialVelocity()
-          particles.current[index].position.x = initialXPosition
-          particles.current[index].position.y = initialYPosition
-          particle.material.opacity = 1
-        }
+          // Reset particle to origin
+          if (particle.material.opacity < 0.1) {
+            particles.current[index].velocity = generateInitialVelocity()
+            // particles.current[index].position.x = initialXPosition
+            if (iPhoneFrameRef.current) {
+              const vFOV = THREE.MathUtils.degToRad(camera.fov)
+              const height = 2 * Math.tan(vFOV / 2) * camera.position.z // visible height at the z=0 plane
+              const worldWidth = height * camera.aspect
+              particles.current[index].position.x =
+                (iPhoneFrameRef.current.getBoundingClientRect().x /
+                  window.innerWidth -
+                  0.5) *
+                worldWidth
+            }
+            particles.current[index].position.y = initialYPosition
+            particle.material.opacity = 1
+          }
 
-        // Bounce off of the floor
-        if (particles.current[index].position.y < floorHeight) {
-          particles.current[index].velocity[1] = Math.abs(
-            particles.current[index].velocity[1] * 0.1,
-          )
-          particles.current[index].position.y = floorHeight
-          particles.current[index].velocity[0] *= 0.8
-        }
+          // Bounce off of the floor
+          if (particles.current[index].position.y < floorHeight) {
+            particles.current[index].velocity[1] = Math.abs(
+              particles.current[index].velocity[1] * 0.1,
+            )
+            particles.current[index].position.y = floorHeight
+            particles.current[index].velocity[0] *= 0.8
+          }
 
-        // Bounce off of the left wall
-        const wallX = 2.385
-        if (
-          particles.current[index].position.x < wallX &&
-          particles.current[index].position.y < 1.6 &&
-          particles.current[index].position.y > -1.25
-        ) {
-          particles.current[index].velocity[0] = Math.abs(
-            particles.current[index].velocity[0] * 0.3,
-          )
-          particles.current[index].position.x = wallX
-          particles.current[index].velocity[1] *= 0.2
-        }
+          // // Bounce off of the left wall
+          // const wallX = 2.385
+          // if (
+          //   particles.current[index].position.x < wallX &&
+          //   particles.current[index].position.y < 1.6 &&
+          //   particles.current[index].position.y > -1.25
+          // ) {
+          //   particles.current[index].velocity[0] = Math.abs(
+          //     particles.current[index].velocity[0] * 0.3,
+          //   )
+          //   particles.current[index].position.x = wallX
+          //   particles.current[index].velocity[1] *= 0.2
+          // }
 
-        // Apply forces
-        particles.current[index].velocity[0] *= xDampening
-        particles.current[index].velocity[1] += gravity
+          // Apply forces
+          particles.current[index].velocity[0] *= xDampening
+          particles.current[index].velocity[1] += gravity
 
-        // integrate velocity
-        particles.current[index].position.x +=
-          particles.current[index].velocity[0]
-        particles.current[index].position.y +=
-          particles.current[index].velocity[1]
+          // integrate velocity
+          particles.current[index].position.x +=
+            particles.current[index].velocity[0]
+          particles.current[index].position.y +=
+            particles.current[index].velocity[1]
 
-        // Assign new positions
-        particle.position.x = particles.current[index].position.x
-        particle.position.y = particles.current[index].position.y
+          // Assign new positions
+          particle.position.x = particles.current[index].position.x
+          particle.position.y = particles.current[index].position.y
 
-        // Gradually dim particles
-        particle.material.opacity *= 0.97
-      })
-    }
-  })
+          // Gradually dim particles
+          particle.material.opacity *= 0.97
+        })
+      }
+    })
 
-  return (
-    <group ref={groupRef}>
-      {particles.current.map((particle, index) => (
-        <Circle
-          key={index}
-          radius={particle.radius}
-          color={particle.color}
-          opacity={particle.opacity}
-          segments={8}
-        />
-      ))}
-    </group>
-  )
-})
+    return (
+      <group ref={groupRef}>
+        {particles.current.map((particle, index) => (
+          <Circle
+            key={index}
+            radius={particle.radius}
+            color={particle.color}
+            opacity={particle.opacity}
+            segments={8}
+          />
+        ))}
+      </group>
+    )
+  },
+)
 
-export default forwardRef(({}, ref) => {
-  return (
-    <div className="h-full w-full absolute z-50">
-      <Canvas className="h-full w-full">
-        <ambientLight intensity={1.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-        <pointLight position={[-10, -10, -10]} />
-        <Particles ref={ref} />
-        <EffectComposer>
-          <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
-        </EffectComposer>
-      </Canvas>
-    </div>
-  )
-})
+export default forwardRef(
+  (
+    {
+      sparksXPosition,
+      iPhoneFrameRef,
+    }: {
+      sparksXPosition: number
+      iPhoneFrameRef: React.RefObject<HTMLDivElement>
+    },
+    ref,
+  ) => {
+    const [widthPortion, setWidthPortion] = useState(0.5)
+
+    useEffect(() => {
+      setWidthPortion(sparksXPosition / window.innerWidth)
+    }, [sparksXPosition])
+
+    return (
+      <div className="h-full w-full absolute z-50">
+        <Canvas className="h-full w-full">
+          <ambientLight intensity={1.5} />
+          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+          <pointLight position={[-10, -10, -10]} />
+          <Particles
+            ref={ref}
+            xOriginPosition={widthPortion}
+            iPhoneFrameRef={iPhoneFrameRef}
+          />
+          <EffectComposer>
+            <Bloom
+              luminanceThreshold={0}
+              luminanceSmoothing={0.9}
+              height={300}
+            />
+          </EffectComposer>
+        </Canvas>
+      </div>
+    )
+  },
+)
