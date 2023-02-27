ce3f373138e11c45dc7d634e7cddf2bc10ddddd9
gibsonbailey <baileytrefts@gmail.com>
2023-02-26 18:29:09 -0800

    Fix prompt on small screens.

--- web/src/components/terminal/Prompt.tsx
+++ web/src/components/terminal/Prompt.tsx
@@ -9,15 +9,15 @@ export default ({
 }) => {
   return (
     <div className="flex border-t border-t-slate-800 p-2 font-medium text-xs sm:text-md">
-      <span className="text-fuchsia-400">~/git-streak-tracker</span>
-      <span className="text-green-300 ml-2">git:(</span>
-      <span className="text-yellow-200">main</span>
-      <span className="text-green-300">
+      <span className="text-fuchsia-400 shrink-0">~/git-streak-tracker</span>
+      <span className="text-green-300 ml-2 shrink-0">git:(</span>
+      <span className="text-yellow-200 shrink-0">main</span>
+      <span className="text-green-300 shrink-0">
         ){linesChanged ? `±${linesChanged}` : null}
       </span>
-      <span className="ml-2">{outputText}</span>
+      <span className="ml-2 truncate">{outputText}</span>
       {showCursor ? (
-        <div className="whitespace-pre bg-cyan-400" style={{ width: 2 }}></div>
+        <span className="whitespace-pre bg-cyan-400" style={{ width: 2 }}></span>
       ) : null}
     </div>
   )
